d33abc3e564ee2fad2b2f26ab5717543ab6c90a6
Matex2395 <mateorgafjo@gmail.com>
2024-11-10 20:53:15 -0500

    CRUD Usuarios

--- Models/AdminMvcContext.cs
+++ Models/AdminMvcContext.cs
@@ -21,7 +21,7 @@ public partial class AdminMvcContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
 //        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Initial Catalog=AdminMVC;User ID=MateoSA;Password=empanada123;Encrypt=True;TrustServerCertificate=True;");
 
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using AdminMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AdminMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("conection")));
 
 var app = builder.Build();
 
